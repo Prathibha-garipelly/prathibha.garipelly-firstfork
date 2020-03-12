383ce4a13e490adf74c2e5b5aacb511260139a0d
admin <admin@bvrit56>
2020-03-12 15:16:50 +0530

    first

---
+++ Samplefork/src/Solution.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Solution {
+	public static void main(String[] args)
+	{
+		System.out.println("hello prathibha");
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter a and b values");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println(c);
+	}
+}
